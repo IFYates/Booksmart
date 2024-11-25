d6973ede1fe7f8a22b3bcb4a1aaa8e2f90028f2e
Ian Yates <ian@iyates.co.uk>
2024-11-25 14:00:54 +0000

    New styling for bookmarks

--- src/code/ui/elements/bookmark.js
+++ src/code/ui/elements/bookmark.js
@@ -44,6 +44,7 @@ export class BookmarkElement extends BaseHTMLElement {
         super(template, ['/styles/common.css', '/styles/bookmark.css', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css'])
         this.#bookmark = bookmark
         this.id = 'bookmark-' + bookmark.id
+        this.classList.add('grid') // TODO: option
     }
 
     onclick(ev) {
